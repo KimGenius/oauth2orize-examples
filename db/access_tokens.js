48e0afa8d262377cffd41a6f6a872c782b89a74a
KimGenius <geniusk1047@naver.com>
2018-01-18 11:54:06 +0900

    findByUserIdAndClientId for of -> for in

--- db/access_tokens.js
+++ db/access_tokens.js
@@ -15,12 +15,12 @@ module.exports.findByUserIdAndClientId = (userId, clientId, done) => {
   console.log(userId)
   console.log(clientId)
 
-  for (let token of Object.keys(tokens)) {
+  for (let token in tokens) {
     console.log('tokens[token].userId : ', tokens[token].userId)
     console.log('userId : ', userId)
     console.log('tokens[token].clientId : ', tokens[token].clientId)
     console.log('clientId : ', clientId)
-    if (tokens[token].userId == userId && tokens[token].clientId == clientId) {
+    if (tokens[token].userId === userId && tokens[token].clientId === clientId) {
       console.log('success!')
       return done(null, token);
     }
